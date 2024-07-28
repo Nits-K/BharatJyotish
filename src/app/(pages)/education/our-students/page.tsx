26c2935661c02ae708990104a708bacc42c3cc3a
Nitin Kant <kantnitin7@gmail.com>
2024-07-28 17:40:58 +0530

    pages added

---
+++ src/app/(pages)/education/our-students/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+const Students = () => {
+  return (
+    <>
+
+      <div className="h-screen bg-gray-100 text-black"> Work in progress</div>
+      
+    </>
+  );
+};
+export default Students;
